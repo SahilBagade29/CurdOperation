24140cc49fcf631ab0495c31717c2a0009d6eb45
Sahil <bagadesahil31@gmail.com>
2024-01-28 01:24:43 +0530

    push a code on Simple Curd Operation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import Favcolor from './component/Favcolor';
+import P3Todo from "./component/Simplecurdoperation";
+
+function App() {
+  return (
+    <>
+    
+     <P3Todo/>
+    
+    </>
+  );
+}
+
+export default App;
